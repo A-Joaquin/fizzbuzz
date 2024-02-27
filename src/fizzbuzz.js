8c5bfda3f1f4b65f1fdbc8c5841472a3f3cd6e41
Arturo <arturo.veliz@ucb.edu.bo>
2024-02-26 20:25:40 -0400

    imprime la cadena con la secuencia de números hasta el límite segun las reglas FizzBuzz

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,18 +1,28 @@
-function generarfizzbuzz(n) {
+export function generarfizzbuzz(n) {
     let mensaje = "";
-    if(n%3==0||n%5==0)
-    {
-        if(n % 3 == 0){
-            mensaje = "fizz";
-        }
-        if(n%5==0)
-        {
-            mensaje=mensaje+"buzz"
-        }
-    }
-    else{
-        mensaje = n + "";
+    if (n % 3 == 0 || n % 5 == 0) {
+      if (n % 3 == 0) {
+        mensaje = "Fizz";
+      }
+      if (n % 5 == 0) {
+        mensaje = mensaje + "Buzz";
+      }
+    } else {
+      mensaje = n + "";
     }
     return mensaje;
   }
-  export default generarfizzbuzz;
+  
+  export function generarfizzbuzzLimite(limite) {
+    let secuencia = "";
+    for (let i = 1; i <= limite; i++) {
+      const resultadoFizzBuzz = generarfizzbuzz(i);
+      secuencia += resultadoFizzBuzz;
+  
+      if (i < limite) {
+        secuencia += ", ";
+      }
+    }
+    return secuencia;
+  }
+  
--- src/fizzbuzz.test.js
+++ src/fizzbuzz.test.js
@@ -1,4 +1,4 @@
-import generarfizzbuzz from "./fizzbuzz.js";
+import { generarfizzbuzz, generarfizzbuzzLimite } from "./fizzbuzz.js";
 
 describe("FizzBuzz", () => {
   it("Deberia generar el mismo numero para uno que no tiene reglas por ejemplo el 1", () => {
@@ -10,24 +10,24 @@ describe("FizzBuzz", () => {
   });
 
   it("Deberia mostrar la palabra fizz para los multiplos del numero 3", () => {
-    expect(generarfizzbuzz(3)).toEqual("fizz");
+    expect(generarfizzbuzz(3)).toEqual("Fizz");
   });
   it("Deberia mostrar la palabra fizz para los multiplos del numero 3", () => {
-    expect(generarfizzbuzz(6)).toEqual("fizz");
+    expect(generarfizzbuzz(6)).toEqual("Fizz");
   });
 
   it("Deberia mostrar la palabra buzz para los multiplos del numero 5", () => {
-    expect(generarfizzbuzz(5)).toEqual("buzz");
+    expect(generarfizzbuzz(5)).toEqual("Buzz");
   });
   it("Deberia mostrar la palabra buzz para los multiplos del numero 5", () => {
-    expect(generarfizzbuzz(10)).toEqual("buzz");
+    expect(generarfizzbuzz(10)).toEqual("Buzz");
   });
 
   it("Deberia mostrar la palabra fizzbuzz para los multiplos del numero 5 y 3", () => {
-    expect(generarfizzbuzz(15)).toEqual("fizzbuzz");
+    expect(generarfizzbuzz(15)).toEqual("FizzBuzz");
   });
   it("Deberia mostrar la palabra fizzbuzz para los multiplos del numero 5 y 3", () => {
-    expect(generarfizzbuzz(30)).toEqual("fizzbuzz");
+    expect(generarfizzbuzz(30)).toEqual("FizzBuzz");
   });
 
 });
